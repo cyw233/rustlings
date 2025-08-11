7d9e0de0a43206545db65eed039ccf619e47ed9f
Chenyang Wang <chenyangw233@gmail.com>
2025-08-11 23:19:39 +1000

    feat: finish all exercises

--- exercises/23_conversions/try_from_into.rs
+++ exercises/23_conversions/try_from_into.rs
@@ -28,14 +28,36 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
 
-    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {}
+    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let (r, g, b) = tuple;
+        if r < 0 || g < 0 || b < 0 || r > 255 || g > 255 || b > 255 {
+            return Err(IntoColorError::IntConversion);
+        }
+
+        Ok(Color {
+            red: r as u8,
+            green: g as u8,
+            blue: b as u8,
+        })
+    }
 }
 
 // TODO: Array implementation.
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
 
-    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {}
+    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let [r, g, b] = arr;
+        if r < 0 || g < 0 || b < 0 || r > 255 || g > 255 || b > 255 {
+            return Err(IntoColorError::IntConversion);
+        }
+
+        Ok(Color {
+            red: r as u8,
+            green: g as u8,
+            blue: b as u8,
+        })
+    }
 }
 
 // TODO: Slice implementation.
@@ -43,7 +65,23 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
 
-    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {}
+    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        match slice {
+            [r, g, b] => {
+                if *r < 0 || *g < 0 || *b < 0 || *r > 255 || *g > 255 || *b > 255 {
+                    return Err(IntoColorError::IntConversion);
+                }
+
+                Ok(Color {
+                    red: *r as u8,
+                    green: *g as u8,
+                    blue: *b as u8,
+                })
+            }
+            _ => Err(IntoColorError::BadLen),
+        }
+
+    }
 }
 
 fn main() {
